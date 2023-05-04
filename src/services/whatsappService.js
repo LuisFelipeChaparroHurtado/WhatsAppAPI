1be17f4ea8562e4f73cbbcd09bf7f2a2ab318761
Felipe <luis.chaparroh@usantoto.edu.co>
2023-05-04 10:06:55 -0500

    Message X7

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -4,7 +4,7 @@ const https = require("https");
 function SendMessageWhatsApp(textResponse, number) {
     const data = JSON.stringify({
         "messaging_product": "whatsapp",
-        "to": number,
+        "to": `whatsapp:${number}`,
         "text": {
             "body": textResponse
         },
